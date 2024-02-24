e5b0de4be81ee067ceded305feb153607783aee6
Maria Alexandra Boros <boros.maria.alexandra@hotmail.com>
2024-02-24 21:36:36 +0200

    mergeee

---
+++ script.js
@@ -0,0 +1,6 @@
+const formOpenBtn = document.querySelector("#form-open"),
+home = document.querySelector(".home"),
+formContainer = document.querySelector(".form_container"),
+formCloseBtn = document.querySelector(".form_close"),
+signupBtn = document.querySelector("#signup"),
+loginBtn = document.querySelector("#signup"),
